a9fd684b54bd1ed4fea6b34c3ab63f9da1b6ff96
macintoshhd <khoinguyenvk9@gmail.com>
2020-07-26 23:55:49 +0700

    put

---
+++ TrangDocTinTuc/NewFeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewFeedViewController.swift
+//  TrangDocTinTuc
+//
+//  Created by Khôi Nguyễn on 7/26/20.
+//  Copyright © 2020 ProjectiOs. All rights reserved.
+//
+
+import UIKit
+
+class NewFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
