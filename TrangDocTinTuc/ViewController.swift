823127d87e254770c876e115763c7e90e296acbb
macintoshhd <khoinguyenvk9@gmail.com>
2020-07-26 22:01:49 +0700

    Initial Commit

---
+++ TrangDocTinTuc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrangDocTinTuc
+//
+//  Created by Khôi Nguyễn on 7/26/20.
+//  Copyright © 2020 ProjectiOs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
